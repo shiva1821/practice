1478c9916bb2c88d1b73980147a0ec508630d9f6
shiva1821 <vishalmudhiraj151@gmail.com>
2026-04-01 17:04:35 +0530

    added files

--- src/accessspecifiers/b.java
+++ src/accessspecifiers/b.java
@@ -4,7 +4,7 @@ public class b {
 	public static void main(String[] args) {
 		System.out.println("b class");
 		System.out.println(a.name);
-		//System.out.println(name1);
+		//System.out.println(name11);
 		//System.out.println(name2);
 	}
 

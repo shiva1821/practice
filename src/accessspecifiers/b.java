64a392c7b428144ad6739587242f2f6840321b9b
shiva1821 <vishalmudhiraj151@gmail.com>
2026-04-01 11:52:30 +0530

    added

---
+++ src/accessspecifiers/b.java
@@ -0,0 +1,11 @@
+package accessspecifiers;
+
+public class b {
+	public static void main(String[] args) {
+		System.out.println("b class");
+		System.out.println(a.name);
+		//System.out.println(name1);
+		//System.out.println(name2);
+	}
+
+}
